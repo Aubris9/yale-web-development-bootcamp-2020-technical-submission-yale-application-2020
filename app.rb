dfe916b2afbce07270394503c2077ff6376e526c
Aubre Ibanez <aui222@lehigh.edu>
2020-04-20 14:06:15 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,8 +1,10 @@
-require_relative './test'
-AVERAGE_HEIGHT_IN_INCHES = 67
+let name = 'Aubre Ibanez' ;
+let age = '20';
+ley favorite_food = 'cereal';
+let height_in_inches = '65';
+AVERAGE_HEIGHT_IN_INCHES = '67';
+end
+# Your Code Here! 
 
-# Your Code Here!
-
-## Do not modify below this line!
-
+## Do not modify below this line 
 output(name, age, favorite_food, height_message)
